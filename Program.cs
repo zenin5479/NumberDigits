674e4a8149920e7202a97b1096cb73da5cb12703
zenin5479 <zenin5479@gmail.com>
2023-08-18 18:27:04 +0300

    Update 0.5 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Globalization;
 
-// Как получить количество цифр, идущих после точки (.)
+// Определить количество цифр, идущих после точки (.)
 // Работает с точкой (.) и не работает с запятой (,)
 
 namespace NumberDigits
@@ -10,17 +9,20 @@ namespace NumberDigits
     {
         private static void Main()
         {
-            // Вариант 1
+
             Console.Write("Введите число: ");
-            // Преобразовать число с плавающей точкой в строку.
+            // Преобразовать число с плавающей точкой в строку
             var input = Convert.ToString(Console.ReadLine());
 
+            // Вариант 1
             if (input != null)
             {
                 var symbol = input.Split('.');
-                // Найти позицию точки в строке. Если точка не найдена - "Не удалось распознать число"
+                // Найти позицию точки в строке
                 var length = symbol.Length > 1 ? symbol[1].Length : 0;
-                Console.WriteLine("Вариант 1: Число содержит {0} символов после запятой", length);
+                // Если точка найдена, найти длину строки после точки
+                var places = (input.Length - 1) - length;
+                Console.WriteLine("Вариант 1: Число содержит {0} символов до точки и {1} символов после точки", places, length);
             }
             else
             {
@@ -28,41 +30,17 @@ namespace NumberDigits
             }
 
             // Вариант 2
-            try
+            if (input != null)
             {
-                // Преобразовать число с плавающей точкой в строку.
-                var numberAsString = input.ToString(CultureInfo.CurrentCulture);
-                var decimalPlaces = 0;
-                // Найти позицию точки в строке методом IndexOf().
-                var decimalPosition = numberAsString.IndexOf('.');
-                // Если точка не найдена, вернуть 0.
-                if (decimalPosition > 0)
-                {
-                    decimalPlaces = (numberAsString.Length - 1) - decimalPosition;
-                }
-                // Если точка найдена, найти длину строки после точки.
-                Console.WriteLine("Вариант 2: Число содержит {0} символов после запятой и {1} символов до запятой", decimalPlaces, decimalPosition);
+                // Найти позицию точки в строке методом IndexOf()
+                var decimalPosition = input.IndexOf('.');
+                // Если точка найдена, найти длину строки после точки
+                var decimalPlaces = (input.Length - 1) - decimalPosition;
+                Console.WriteLine("Вариант 2: Число содержит {0} символов до точки и {1} символов после точки", decimalPosition, decimalPlaces);
             }
-            catch (Exception e)
+            else
             {
-                Console.WriteLine(e);
                 Console.WriteLine("Не удалось распознать число");
-                throw;
-            }
-
-            // Вариант 3
-            try
-            {
-                var str = input.ToString(CultureInfo.InvariantCulture);
-                var countBefore = str.IndexOf('.');
-                var countAfter = (str.Length - 1) - countBefore;
-                Console.WriteLine("Вариант 3: Число содержит {0} символов после запятой и {1} символов до запятой", countAfter, countBefore);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                Console.WriteLine("Не удалось распознать число");
-                throw;
             }
         }
     }
