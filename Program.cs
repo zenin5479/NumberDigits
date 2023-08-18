9bde28d295573de37c2126b8e0a169c585f988a3
zenin5479 <zenin5479@gmail.com>
2023-08-18 11:12:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace NumberDigits
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            const decimal number = (decimal)10.12345454;
+            var numberAsString = number.ToString(CultureInfo.InvariantCulture);
+            var decimalPlaces = 0;
+            var decimalPosition = numberAsString.IndexOf('.');
+
+            if (decimalPosition > 0)
+            {
+                decimalPlaces = numberAsString.Length - decimalPosition - 1;
+            }
+
+            Console.WriteLine(decimalPlaces);
+        }
+    }
+}
