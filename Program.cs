d65767c0b62f02bc29d65fb73431c9077aea6bd2
zenin5479 <zenin5479@gmail.com>
2023-08-29 10:26:32 +0300

    Update 0.9 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -15,36 +15,38 @@ namespace NumberDigits
             var inputLength = input.Length;
 
             // Вариант 1
+            var symbolOne = input.Split('.');
+            // Найти позицию точки в строке
+            var lengthOne = symbolOne.Length > 1 ? symbolOne[1].Length : 0;
+            if (lengthOne == 0)
             {
-                var symbolOne = input.Split('.');
-                // Найти позицию точки в строке
-                var lengthOne = symbolOne.Length > 1 ? symbolOne[1].Length : 0;
-                if (lengthOne == 0)
-                {
-                    Console.WriteLine("Вариант 1: Число содержит {0} символов", inputLength);
-                }
-                else
-                {
-                    // Если точка найдена, найти длину строки после точки
-                    var placesOne = (input.Length - 1) - lengthOne;
-                    Console.WriteLine("Вариант 1: Число содержит {0} символов до точки и {1} символов после точки длина числа {2} символов", placesOne, lengthOne, inputLength);
-                }
+                Console.WriteLine("Вариант 1: Число содержит {0} символов", inputLength);
+            }
+            else
+            {
+                // Если точка найдена, найти длину строки после точки
+                var placesOne = (input.Length - 1) - lengthOne;
+                Console.WriteLine(
+                    "Вариант 1: Число содержит {0} символов до точки и {1} символов после точки длина числа {2} символов",
+                    placesOne, lengthOne, inputLength);
             }
 
+
+
             // Вариант 2
+            // Найти позицию точки в строке методом IndexOf()
+            var symbolTwo = input.IndexOf('.');
+            if (symbolTwo == -1)
             {
-                // Найти позицию точки в строке методом IndexOf()
-                var symbolTwo = input.IndexOf('.');
-                if (symbolTwo == -1)
-                {
-                    Console.WriteLine("Вариант 1: Число содержит {0} символов", inputLength);
-                }
-                else
-                {
-                    // Если точка найдена, найти длину строки после точки
-                    var placesTwo = (inputLength - 1) - symbolTwo;
-                    Console.WriteLine("Вариант 2: Число содержит {0} символов до точки и {1} символов после точки длина числа {2} символов", symbolTwo, placesTwo, inputLength);
-                }
+                Console.WriteLine("Вариант 1: Число содержит {0} символов", inputLength);
+            }
+            else
+            {
+                // Если точка найдена, найти длину строки после точки
+                var placesTwo = (inputLength - 1) - symbolTwo;
+                Console.WriteLine(
+                    "Вариант 2: Число содержит {0} символов до точки и {1} символов после точки длина числа {2} символов",
+                    symbolTwo, placesTwo, inputLength);
             }
         }
     }
